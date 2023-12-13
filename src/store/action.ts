b426253355b7a348cd0a23ca153c52b18d4acfc0
Asiya Ermolaeva <asiya_ermolaeva@mail.ru>
2023-12-13 22:20:36 +0300

    fix type

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,5 @@
 import {createAction} from '@reduxjs/toolkit';
-import { Film} from '../types';
+import { Film, PromoFilm} from '../types';
 
 
 export const setActiveGenre = createAction<string>('genre/setActiveGenre');
@@ -8,6 +8,6 @@ export const setFilms = createAction<Film[]>('genre/setFilms');
 
 export const loadFilms = createAction<Film[]>('list/loadFilms');
 
-export const loadPromo = createAction<Film[]>('list/loadPromo');
+export const loadPromo = createAction<PromoFilm>('list/loadPromo');
 
 export const setFilmsDataLoadingStatus = createAction<boolean>('data/setFilmsDataLoadingStatus');
