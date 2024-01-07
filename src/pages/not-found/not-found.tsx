81b422aa5a41a8d7de2ff72a9db2341ff08e7770
Asiya Ermolaeva <asiya_ermolaeva@mail.ru>
2024-01-07 22:09:04 +0300

    change the text and style on 404 page

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -6,13 +6,10 @@ function NotFound(){
   return (
     <div className='not-found-container'>
       <h1 className='not-found-title'>404</h1>
-      <h2 className='not-found-sub-title'>UH OH! You&apos;re lost.</h2>
       <p className='not-found-message'>
-        The page you are looking for does not exist.<br/>
-        How you got here is a mystery.<br/>
-        But you can click the button below to go back to the homepage.
+        Ooops...<br/>
       </p>
-      <Link to={AppRoutes.Main} className='not-found-btn' data-testid='not-found-btn'>HOME</Link>
+      <Link to={AppRoutes.Main} className='not-found-btn' data-testid='not-found-btn'>TO HOME PAGE</Link>
     </div>
   );
 }
